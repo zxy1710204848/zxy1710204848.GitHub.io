754b3b143dd5eaf4d39245b3503e15fe92a8cc81
WADE <scyxz6@nottingham.edu.cn>
2024-01-26 17:20:47 +0000

    back

--- create.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS information;
-
-CREATE TABLE information(
-	ID INTEGER NOT NULL PRIMARY KEY,
-	content VARCHAR(200) NOT NULL
-);
-
-INSERT INTO information (content) VALUES ("No order post");
